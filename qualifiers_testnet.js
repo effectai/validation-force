26da78bf9510487d1140755c56e0af594ed83092
djmbritt <david@effect.ai>
2022-11-24 10:34:59 +0100

    Add pirate quali
    Signed-off-by: djmbritt <david@effect.ai>

--- qualifiers_testnet.js
+++ qualifiers_testnet.js
@@ -4,6 +4,15 @@ const campaign14 = await readFile('qualis/testnet/14_human.string.js', 'utf8')
 
 export default [
     { 
+        "campaign_id": 4,
+        "approve_qualification_id": 4,
+        "reject_qualification_id": 5,
+        "validate_function": "return submission.toLowerCase() === answer.toLowerCase();",
+        "auto_loop": true,
+        "threshold": 0.8,
+        "answers": {"identity":"roger","alphabet":"x","bird":"Parrot","peterpan":"RobinWilliams","shiver":"timbers","curse":"pearl","timespent":"oceans","realpirate":"BlackBeard","dangerlaugh":"facedanger","swabdecks":"cleanfloors","hoistsails":"moveboat","blackbeardshortbeard":"beardno","famouscapn":"famousyes","scallywags":"scallyno","strumpets":"strumpetyes"}
+    },
+    { 
         "campaign_id": 9,
         "approve_qualification_id": 25,
         "reject_qualification_id": 25,
