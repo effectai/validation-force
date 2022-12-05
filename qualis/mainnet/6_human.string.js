111037c9f340a812d3ccdc5b89f1ac6d9ea75c0f
djmbritt <david@effect.ai>
2022-12-05 18:29:33 +0100

    Update format for validation scripts

--- qualis/mainnet/6_human.string.js
+++ qualis/mainnet/6_human.string.js
@@ -2,18 +2,21 @@
 // imports need to use require. import is not supported
 // Also make sure that the import is present in index.js
 
-const { verifyCaptcha } = require("./captcha.js").verifyCaptcha
+const tmpString = `
+const { verifyCaptcha } = (await import('./captcha.js')).default
 const s = submissions;
 const a = answers;
 console.log('🔥submission / answers', s, a);
-const captchaPath = `${forceInfo.campaignId}${forceInfo.batchId}${forceInfo.accountId}${forceInfo.submissionId}`
+const captchaPath = "" + forceInfo.campaignId + forceInfo.batchId + forceInfo.accountId + forceInfo.submissionId
 console.log('🎏🎏🎏🎏', 'captchaPath', captchaPath, 'submissions.captchainput', submissions.captchainput);
 const isValid = verifyCaptcha(captchaPath, submissions.captchainput);
-console.log(`isValid: ${isValid}, captchaPath: ${captchaPath}, captchaInput: ${submissions.captchainput}`);
+console.log("isValid:", isValid, "captchaPath:", captchaPath, "captchaInput:", submissions.captchainput);
 if (isValid && a.calcmul === s.calcmul && parseInt(s.humanSlider) >= parseInt(a.humanSlider))
     return { value: true, quali_value: 'human' }
 else {
     return { value: false, quali_value: 'bot' }
 }
+`
 
+export default String(tmpString)
 
--- qualis/mainnet/8_twitter.string.js
+++ qualis/mainnet/8_twitter.string.js
@@ -2,10 +2,11 @@
 // imports need to use require. import is not supported
 // Also make sure that the import is present in index.js
 
-const fetch = require('cross-fetch');
+const newLocal = `
+const fetch = (await import('cross-fetch')).default;
 
-const response = await fetch(`https://api.twitter.com/2/users/by/username/${submissions.twitter_handle}`, {
-    headers: { 'Authorization': `Bearer ${process.env.TWITTER_BEARER}` }
+const response = await fetch("https://api.twitter.com/2/users/by/username/" + submissions.twitter_handle, {
+    headers: { 'Authorization': "Bearer " + process.env.TWITTER_BEARER }
 });
 
 const body = await response.json();
@@ -16,7 +17,12 @@ await new Promise(resolve => setTimeout(resolve, 1000));
 
 console.log(body);
 if (!body.data || !body.data.id) {
+    console.log("Rejecting from twitter", submissions.twitter_handle);
     return { value: false, quali_value: submissions.twitter_handle }
 } else {
+    console.log("Accepted from twitter", submissions.twitter_handle);
     return { value: true, quali_value: submissions.twitter_handle }
 }
+`
+
+export default String(newLocal)
